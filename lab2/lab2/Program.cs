4aacbfbded8a71f74fd6132d8c7e8de2ed15d71d
KathyPrim <eeprimakova@stud.etu.ru>
2021-02-20 22:59:01 +0500

    1 & 2

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -4,12 +4,12 @@ namespace lab2
 {
     enum AccountType
     {
-        Credit,
-        Deposit,
+        Credit = 100,
+        Deposit = 200,
     }
     struct Account
     {
-        public long accNu;
+        public long accNo;
         public decimal accBal;
         public AccountType accType;
     }
@@ -17,16 +17,16 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             AccountType Golden, Platinum;
             Golden = AccountType.Credit;
             Platinum = AccountType.Deposit;
             Console.WriteLine(Golden);
             Console.WriteLine(Platinum);
             Account Me;
-            Me.accNu = 1010101010;
+            Me.accNo = 1010101010;
             Me.accBal = 100000000000010000;
             Me.accType = AccountType.Deposit;
+            Console.WriteLine("Number is {0}, Balance is {1}, Type is {2}", Me.accNo, Me.accBal, Me.accType);
         }
     }
 }
