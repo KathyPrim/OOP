718e67a5ae7894b54a6afcd32c754d5277db3671
KathyPrim <eeprimakova@stud.etu.ru>
2021-02-20 18:21:09 +0500

    1

---
+++ lab1/lab1/greeting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1
+{
+    class greeting
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int i, j;
+            Console.WriteLine("Please, enter two numbers with enter as a divider");
+            name = Console.ReadLine();
+            i = Convert.ToInt32(name);
+            name = Console.ReadLine();
+            j = Convert.ToInt32(name);
+            int k;
+            try
+            {
+                k = i / j;
+                Console.WriteLine("Result is {0}", k);
+            }
+            catch
+            {
+                Console.WriteLine("Exception!");
+            }
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab2
+{
+    enum AccountType
+    {
+        Credit,
+        Deposit,
+    }
+    struct Account
+    {
+        public long accNu;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AccountType Golden, Platinum;
+            Golden = AccountType.Credit;
+            Platinum = AccountType.Deposit;
+            Console.WriteLine(Golden);
+            Console.WriteLine(Platinum);
+            Account Me;
+            Me.accNu = 1010101010;
+            Me.accBal = 100000000000010000;
+            Me.accType = AccountType.Deposit;
+        }
+    }
+}
